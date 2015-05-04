49432ee61bbc7a97bcf6812cfe806e59ef8db3c4
Lucian Kahn <luciankahn@gmail.com>
2015-05-04 10:33:28 -0400

    DUN.

--- widget.js
+++ widget.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('#input').keyup(function(event) {
     var inputHTML = markdown.toHTML($('#input').val());
     console.log(inputHTML);
-    // $('#output').text
+    document.getElementById("output").innerHTML = inputHTML;
   })
 });
